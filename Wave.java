aff80f29c7343e2d705fcc742028e3b2cdb02dea
Dominik-Hillmann <dominik.hillmann@gmx.de>
2018-05-14 20:58:07 +0200

    looking at variables around sin

--- Wave.java
+++ Wave.java
@@ -4,6 +4,8 @@ public class Wave extends PApplet {
 	private final int WIDTH = 700;
 	private final int HEIGHT = 1000;
 	
+	private double spikedness = 500;
+	
 	public void settings() {
 		size(WIDTH, HEIGHT);
 	}
@@ -14,7 +16,22 @@ public class Wave extends PApplet {
 	
 	public void draw() {
 		fill(0);
-		ellipse((float) (WIDTH / 2), (float) (HEIGHT / 2), 200f, 200f);
+		
+		// if (spikedness <= 500) hin und her
+				
+		for (int x = 1; x <= WIDTH; x++) {
+			line(				
+				(float) x,
+				sin(x, 0.05, 500, 300),
+				(float) x + 1,
+				sin(x + 1, 0.05, 500, 300)				
+			);
+		}
+	}
+	
+	
+	private float sin(int x, double wideness, double spikedness, int addPx) {
+		return (float) (spikedness * Math.sin(wideness * x) + addPx);
 	}
 	
 	
