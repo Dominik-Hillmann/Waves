f63b491934d646dcd2f8b753871df86f5b837bdc
Dominik-Hillmann <dominik.hillmann@gmx.de>
2018-06-23 11:38:04 +0200

    Add files via upload

--- waves.js
+++ waves.js
@@ -1,16 +1,10 @@
-const NUM_WAVES = 3;
-const PERC_WIDTH = 0.38;
-// const SPEED_WINDOW = { upper: .01, lower: .01, step: .0001 };
-// const WIND_WINDOW = { upper: .00001, lower: .01, step: .0001 };
-// const SPEED_WINDOW = { upper: .01, lower: .01, step: .0001 };
-
 class Color {
    constructor(r, g, b) {
       this.r = r;
       this.g = g;
       this.b = b;
    }
-}
+} // Color
 
 class Slider {
    // A slider ping-pongs a value between its min and its max.
@@ -20,7 +14,8 @@ class Slider {
       this.mean = Math.round((min + max) / 2);
 
       this.step = step;
-      this.current = this.randBetween(min, max); // RANDOM STARTER EINBAUEN
+      // value starts at a random position within range => curves will almost always look a bit different
+      this.current = this.randBetween(min, max);
 
       this.down = true;
       this.curFrame = 0;
@@ -28,6 +23,7 @@ class Slider {
 
    curr(frame) {
       // returns current value and updates its position between min and max
+      // the check for the frame is useful to not update the slider several times within one
       if (this.curFrame != frame) {
          this.current += this.down ? -this.step : this.step;
          this.down = (this.current < this.min || this.current > this.max) ? !this.down : this.down;
@@ -44,7 +40,8 @@ class Slider {
 
 class Wave {
    constructor(speed, meanHeight, wind, spike) {
-      // later replacement of magic numbers
+      // I know it is ugly, but these magic numbers needed to be put in by hand in order to see what looks good
+      // the sliders ping-pong the parameters around a sin curve and make its movement look somewhat natural or random
       this.speed = new Slider(speed - 0.01, speed + 0.01, 0.0001);
       this.vert = 0;
 
@@ -65,37 +62,54 @@ class Wave {
 } // Wave
 
 
-let cnv;
+let cnv; // needs to visible within draw() and setup(), therefore cannot be declared there
 let waves = [];
-let colors = [
-   new Color(163, 201, 168),
-   new Color(105, 162, 151),
-   new Color(80, 128, 142)
-]; // same # of colors as  NUM_WAVES
+let possibleColors = [[
+      // green-blue
+      new Color(163, 201, 168),
+      new Color(105, 162, 151),
+      new Color(80, 128, 142)
+   ], [
+      // green
+      new Color(49, 87, 44),
+      new Color(79, 119, 45),
+      new Color(144, 169, 85)
+   ], [
+      // reds
+      new Color(100, 13, 20),
+      new Color(128, 14, 19),
+      new Color(209, 0, 41)
+   ]
+];
 
+let colors = possibleColors[Math.floor(Math.random() * possibleColors.length)];
+
+const NUM_WAVES = colors.length;
+const PERC_WIDTH = 0.38;
+
+// functions for p5
 function setup() {
    frameRate(30);
    cnv = createCanvas(Math.round(PERC_WIDTH * displayWidth), displayHeight);
    cnv.parent("sketch");
 
    for (let i = 0; i < NUM_WAVES; i++) {
+      // again magic numbers, needed to know what intuitively looks good
       waves.push(new Wave(.01, height / 2, .01, 40));
    }
-}
+} // setup()
 
 function draw() {
-   background(255, 255, 255, 10);
+   background(255, 255, 255/*, 20*/);
 
    for (let i = 0; i < NUM_WAVES; i++) {
       let col = colors[i];
-      // fill(col.r, col.g, col.b);
       stroke(col.r, col.g, col.b);
+      fill(col.r, col.g, col.b);
       waves[i].drawWave(frameCount, 2);
    }
-}
+} // draw()
 
 function resize() {
    resizeCanvas(Math.round(PERC_WIDTH * windowWidth), windowHeight);
-   /**** WICHTIG: Sketch soll stoppen, sobald Fenster zu klein wird / neu starten, wenn wieder richtige
-    Größe erreicht ist****/
-}
+} // resize()
